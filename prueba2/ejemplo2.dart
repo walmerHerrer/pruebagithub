9d737670c7e8f558b1ec52ec55348b24a0fbfb06
walmerHerrer <0512018020@alumnos.unp.edu.pe>
2023-11-02 09:23:18 -0500

    prueba 2

---
+++ prueba2/ejemplo2.dart
@@ -0,0 +1,8 @@
+class ejemplo2 extends StatelessWidget {
+  const ejemplo2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
