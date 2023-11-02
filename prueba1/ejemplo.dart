946249681a46d39780fd21e217527342166ac9cc
walmerherrer <0512018020@alumnos.unp.edu.pe>
2023-11-02 08:57:24 -0500

    primera parte

---
+++ prueba1/ejemplo.dart
@@ -0,0 +1,13 @@
+class primercomponente extends StatefulWidget {
+  const primercomponente({Key? key}) : super(key: key);
+
+  @override
+  State<primercomponente> createState() => _primercomponenteState();
+}
+
+class _primercomponenteState extends State<primercomponente> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
